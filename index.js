02fd2107daa9626204d3f9f5989656d50a4a5a32
Dinesh <dinesh.webappdev@gmail.com>
2022-12-26 15:50:42 +0530

    new commit

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// import routes 
+// const userRoutes = require('./routes/user.route')
+// const projectRoutes = require('./routes/project.route')
+// const taskRoutes = require('./routes/task.route')
+
+// app.use('/api/v1/user', userRoutes)
+// app.use('/api/v1/project', projectRoutes)
+// app.use('/api/v1/task', taskRoutes)
 
 module.exports = app
